6ba4989e1adb4ebd7460170d5edf52e67649bb27
morsruth <moresruthantonette@gmail.com>
2023-07-12 11:32:45 +0800

    update

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 export { default as Navbar } from './Navbar/Navbar';
 export { default as PirateFlag } from './PirateFlag/PirateFlag';
-export { default as Character } from './Character/Character ';
+export { default as Character } from './Character/Character';
 export { default as Footer } from './Footer/Footer';
 export { default as SubHeading } from './SubHeading/SubHeading';
 export { default as Rating } from './Rating/Rating';
